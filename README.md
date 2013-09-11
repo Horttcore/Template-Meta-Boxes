f214dea893cb6eee241675b02429c55d2a42ce41
Ralf Hortt <me@horttcore.de>
2013-09-11 11:48:02 +0200

    First release
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 Template-Meta-Boxes
 ===================
 
+## Description
+
 Display meta boxes depending on the selected template
+
+## Installation
+
+* I recommend to ship this file within your template as an include
+* Or put the plugin file in your plugin directory and activate it in your WP backend.
+
+## Usage
+
+* Register any meta box as usual with the WorPress API ( http://codex.wordpress.org/Function_Reference/add_meta_box )
+* Add or remove meta boxes in your `functions.php`
+
+### Remove the post thumbnail meta box for the default page template
+
+`$Template_Boxes = Template_Meta_Boxes::get_instance();
+$Template_Boxes->remove_meta_box( 'default', 'postimagediv', 'page', 'side' );`
+
+### Add the post thumbnail meta box only for the default page template
+
+`$Template_Boxes = Template_Meta_Boxes::get_instance();
+$Template_Boxes->add_meta_box( 'default', 'postimagediv', 'page', 'side' );`
---
+++ template-meta-boxes.php
@@ -0,0 +1,200 @@
+<?php
+/*
+Plugin Name: Template Meta Boxes
+Plugin URI: http://horttcore.de
+Description: Display meta boxes depending on the selected template
+Version: 1.0
+Author: Ralf Hortt
+Author URI: http://horttcore.de
+License: GPL2
+*/
+
+
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+
+
+/**
+* Template Meta Boxes
+*/
+class Template_Meta_Boxes
+{
+
+
+
+	/** Refers to a single instance of this class. */
+	private static $instance = null;
+
+	/** Stores added meta boxes */
+	private $add_meta_boxes = array();
+
+	/** Stores removed meta boxes */
+	private $remove_meta_boxes = array();
+
+
+
+	/**
+	 * Contstructor
+	 *
+	 * @since v1.0
+	 * @author Ralf Hortt
+	 */
+	public function __construct()
+	{
+		add_action( 'do_meta_boxes', array( $this, 'do_meta_boxes' ) );
+	}
+
+
+	/**
+	 * Add a meta box for a special template
+	 *
+	 * @param str $template_name Template name
+	 * @param str $meta_box_id Meta Box ID
+	 * @since v1.0
+	 * @author Ralf Hortt
+	 */
+	public function add_meta_box( $template_name, $meta_box_id, $page, $context )
+	{
+
+		$this->add_meta_boxes[$template_name][] = array( 'id' => $meta_box_id, 'page' => $page, 'context' => $context );
+
+	} // end add_meta_box
+
+
+
+	public function add_template_meta_boxes( $current_template )
+	{
+
+		global $wp_meta_boxes, $post;
+
+		if ( !$wp_meta_boxes || empty( $this->add_meta_boxes ) || !$post )
+			return;
+
+		foreach ( $this->add_meta_boxes as $template => $meta_boxes ) :
+
+			foreach ( $meta_boxes as $key => $meta_box ) :
+
+				if ( $current_template == $template || !isset( $wp_meta_boxes[$meta_box['page']][$meta_box['context']] ) )
+					break;
+
+				if (
+						isset( $wp_meta_boxes[$meta_box['page']][$meta_box['context']]['core'][$meta_box['id']] ) ||
+						isset( $wp_meta_boxes[$meta_box['page']][$meta_box['context']]['default'][$meta_box['id']] ) ||
+						isset( $wp_meta_boxes[$meta_box['page']][$meta_box['context']]['high'][$meta_box['id']] ) ||
+						isset( $wp_meta_boxes[$meta_box['page']][$meta_box['context']]['low'][$meta_box['id']] )
+				)
+					remove_meta_box( $meta_box['id'], $meta_box['page'], $meta_box['context'] );
+
+			endforeach;
+
+		endforeach;
+
+	} // end add_template_meta_boxes
+
+
+	public function do_meta_boxes()
+	{
+
+		global $post;
+
+		$screen = get_current_screen();
+
+		if ( !$screen || 'post' != $screen->base )
+			return;
+
+		$template_name = ( $post->ID ) ? $this->get_template( $post->ID ) : 'default';
+
+		$this->remove_template_meta_boxes( $template_name);
+		$this->add_template_meta_boxes( $template_name );
+
+	} // end do_meta_boxes
+
+
+
+	/**
+    * Creates or returns an instance of this class.
+    *
+    * @return  Foo A single instance of this class.
+    */
+   public static function get_instance() {
+
+		if ( null == self::$instance )
+			self::$instance = new self;
+
+		return self::$instance;
+
+   } // end get_instance;
+
+
+
+   /**
+    * Rempve a meta box for a special template
+    *
+    * @access protected
+    * @param int $post_id Post ID
+    * @return str Template ID
+    * @since v1.0
+    * @author Ralf Hortt
+    */
+   protected function get_template( $post_id )
+   {
+
+   		return get_post_meta( $post_id, '_wp_page_template', TRUE );
+
+   } // end get_template
+
+
+
+   /**
+    * Rempve a meta box for a special template
+    *
+    * @access public
+    * @param str $template_name Template name
+    * @param str $meta_box_id Meta Box ID
+    * @param str $context Context
+    * @since v1.0
+    * @author Ralf Hortt
+    */
+	public function remove_meta_box( $template_name, $meta_box_id, $page, $context )
+	{
+
+		$this->remove_meta_boxes[$template_name][] = array( 'id' => $meta_box_id, 'page' => $page, 'context' => $context );
+
+	} // end remove_meta_box
+
+
+
+	/**
+	 * Remove meta boxes
+	 *
+	 * @access public
+	 * @param str $current_template Current template id
+	 * @since v1.0
+	 * @author Ralf Hortt
+	 **/
+	private function remove_template_meta_boxes( $current_template )
+	{
+
+		if ( !$this->remove_meta_boxes || !isset( $this->remove_meta_boxes[$current_template] ) || empty( $this->remove_meta_boxes[$current_template] ) )
+			return;
+
+		foreach ( $this->remove_meta_boxes[$current_template] as $meta_box ) :
+
+			remove_meta_box( $meta_box['id'], $meta_box['page'], $meta_box['context'] );
+
+		endforeach;
+
+	} // end remove_meta_boxes
+
+
+
+}
+
+$Template_Boxes = Template_Meta_Boxes::get_instance();
+$Template_Boxes = Template_Meta_Boxes::get_instance();
+$Template_Boxes->add_meta_box( 'default', 'postimagediv', 'page', 'side' );
